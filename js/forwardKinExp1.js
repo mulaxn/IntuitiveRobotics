8dc658b5838498e699abc01ceddc77918c0fa868
subhan-haq <shaq@mun.ca>
2025-04-10 11:32:58 -0230

    made some changes and edits. think they are good

--- js/forwardKinExp1.js
+++ js/forwardKinExp1.js
@@ -43,45 +43,6 @@ exp =
     <br></br>
 
     To calculate linear velocity, first we choose any point, q, on the axis of rotation.
-    Then we will take the cross product between -ω and q. This will result in the 
-    linear velocity. Ex, ω = (1,0,0) and q = (0,0,0) -> -ω x q = (0,0,0).
-
-    <br></br>
-
-    Prismatic: 
-    
-    <br></br>
-
-    Because prismatic joints don't rotate their angle doesn't change, therefore 
-    they have no angular velocity.
-
-    <br></br>
-
-    To calculate linear velocity, we treat the direction the joint is extending
-    or contracting in as a unit vector. So, if the joint is moving in the z-axis
-    relative to the reference frame, γ = (0,0,1).
-
-    <br></br>
-
-    Now we must use the product of exponentials formula to calculate the position
-    and orientation of the end-effector after the set of theta values is applied.
-
-    <br></br>
-
-    P.O.E: T(θ) = e^([S1]θ1) e^([S2]θ2) ... e^([Sn]θn) M
-
-    <br></br>
-    <pre>
-    e^[Si] = [e^[ωi]θi   (I - e^[ωi]θi)γi]
-             [   0              1        ]</pre>
-
-    e^[ωi] = I + sin(θ)[ωi] + (1 - cos(θ))[ωi]^2, where [ωi] is the skew-symetric
-    matrix related to ωi.
-
-    <br></br>
-
-    Once we complete this process for all joints we multiply all of our matrices
-    together according to the formula and get our answer.
 </p>`
 
-explanation.innerHTML = exp;
+explanation1.innerHTML = exp;
